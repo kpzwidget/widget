d137dd5e94980767563fd6973ac46f9f81c86bad
kpzwidget <137429305+kpzwidget@users.noreply.github.com>
2023-06-22 23:18:06 +0700

    Add files via upload

---
+++ db.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+    HOST: "dpg-cia77otgkuvusaokgbv0-a",
+    USER: "grady",
+    PORT: 5432,
+    PASSWORD: "m8mQRE0V09ZUI5jKYAMCXTUTdGQwMYDz",
+    DB: "kpzdb",
+    dialect: "postgres",
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    }
+};
+
+// module.exports = {
+//     HOST: "localhost",
+//     USER: "postgres",
+//     PORT: 5432,
+//     PASSWORD: "postgres",
+//     DB: "boomboom",
+//     dialect: "postgres",
+//     pool: {
+//         max: 5,
+//         min: 0,
+//         acquire: 30000,
+//         idle: 10000
+//     }
+// };
